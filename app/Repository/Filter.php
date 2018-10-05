39d6bfd8b98c114328a752de2566a35b8f9292b1
hichigo213 <42939715+hichigo213@users.noreply.github.com>
2018-10-05 16:31:04 +0300

    1.4

---
+++ app/Repository/Filter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+use App\Student;
+use Illuminate\Support\Facades\DB;
+
+Class Filter
+{
+//public function MarkAvg(Student $students, $temp) {
+//    foreach ($students as $student) {
+//        $temp[$student->id] = DB::table('marks')
+//            ->select('subject_id', 'student_id', DB::raw('AVG(mark)as "mark"'))
+//            ->where('student_id', $student->id)
+//            ->groupBy('subject_id')
+//            ->get();
+//    }
+//    endforeach
+//    return $temp;
+//}
+
+
+}
